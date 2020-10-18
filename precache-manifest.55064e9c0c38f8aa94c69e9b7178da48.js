01c3711a9660ac0cb4b68e123e28540fa4abdf52
sean <xiaoxiaojing@coding.net>
2020-10-18 16:31:39 +0800

    Updates

---
+++ precache-manifest.55064e9c0c38f8aa94c69e9b7178da48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad20845530fadc93918adeebbe77fa2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f0df7a463f4dc9bf5607",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7357af019cfbac6eb3cf",
+    "url": "./static/js/2.9e7789ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9e7789ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0df7a463f4dc9bf5607",
+    "url": "./static/js/main.f225874d.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
