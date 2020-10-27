ae15bb467ea10e5136063599de313cdd087caa96
sean <xiaoxiaojing@coding.net>
2020-10-27 09:52:25 +0800

    style

--- src/Home/Nav3.jsx
+++ src/Home/Nav3.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import TweenOne from 'rc-tween-one';
-import { Menu } from 'antd';
-import { getChildrenToRender } from './utils';
+import React from "react";
+import TweenOne from "rc-tween-one";
+import { Menu } from "antd";
+import { getChildrenToRender } from "./utils";
 
 const { Item, SubMenu } = Menu;
 
@@ -73,16 +73,16 @@ class Header3 extends React.Component {
     return (
       <TweenOne
         component="header"
-        animation={{ opacity: 0, type: 'from' }}
+        animation={{ opacity: 0, type: "from" }}
         {...dataSource.wrapper}
         {...props}
       >
         <div
           {...dataSource.page}
-          className={`${dataSource.page.className}${phoneOpen ? ' open' : ''}`}
+          className={`${dataSource.page.className}${phoneOpen ? " open" : ""}`}
         >
           <TweenOne
-            animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }}
+            animation={{ x: -30, type: "from", ease: "easeOutQuad" }}
             {...dataSource.logo}
           >
             <img width="100%" src={dataSource.logo.children} alt="img" />
@@ -109,10 +109,10 @@ class Header3 extends React.Component {
                     duration: 300,
                     onComplete: (e) => {
                       if (this.state.phoneOpen) {
-                        e.target.style.height = 'auto';
+                        e.target.style.height = "auto";
                       }
                     },
-                    ease: 'easeInOutQuad',
+                    ease: "easeInOutQuad",
                   }
                 : null
             }
@@ -120,8 +120,8 @@ class Header3 extends React.Component {
             reverse={!!phoneOpen}
           >
             <Menu
-              mode={isMobile ? 'inline' : 'horizontal'}
-              defaultSelectedKeys={['sub0']}
+              mode={isMobile ? "inline" : "horizontal"}
+              defaultSelectedKeys={["sub0"]}
               theme="light"
             >
               {navChildren}
